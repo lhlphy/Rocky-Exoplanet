5f796dff27108643d44c6a5308269f326c49aa65
dell <lhlphy@qq.com>
2024-07-18 20:33:55 +0800

    Full spectrum main

---
+++ Full_spectrum_main.py
@@ -0,0 +1,23 @@
+import numpy as np
+from Full_spectrum import Full_spectrum
+import argparse
+import warnings
+from scipy.integrate._quadpack_py import IntegrationWarning 
+
+# 忽略 IntegrationWarning  
+warnings.filterwarnings('ignore', category=IntegrationWarning) 
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--id', default="0" , type=str)
+    # parser.add_argument('--coarse', default=0 , type=float)
+    # parser.add_argument('--specular', default=0.5 , type=float)
+    # parser.add_argument('--diffuse', default=0.5 , type=float)
+    parser.add_argument('--Albedo', default=0.5 , type=float)
+    parser.add_argument('--Ntheta', default=5 , type=int)
+    parser.add_argument('--Nwave', default=1 , type=int)
+    args = parser.parse_args()
+    Wavelength_bound = [200, 5000] *1e-9   # Wavelength range (m)
+
+    Full_spectrum(Wavelength_bound, args)
+    
--- run.sh
+++ run.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 #SBATCH --job-name=PythonTest
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks-per-node=64
 #SBATCH --cpus-per-task=1
-#SBATCH --partition=debug
+#SBATCH --partition=wzhcnormal
+#SBATCH -o log/%j.loop
+#SBATCH -e log/%j.loop
+
+. /public/software/apps/anaconda3/5.2.0/etc/profile.d/conda.sh
+
+echo "Maxwell"
 
 module load apps/anaconda3/5.2.0
 conda activate test
 
-python  demo3.py --id 0
-
-
+python  Full_spectrum_main.py --id 0 --Albedo 1 --Ntheta 5 --Nwave 200
 
