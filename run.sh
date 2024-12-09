c9057d813b561dd92b114859dc076bfeb84b5031
dell <lhlphy@qq.com>
2024-12-09 18:43:31 +0800

    log

--- run.sh
+++ run.sh
@@ -29,8 +29,8 @@ export lavatype=zero
 # python  lib/Full_spectrum_main.py --id 9 --Ntheta 15 --Nwave 5 --LB 2.99 --UB 3.01 --mode PC --lavatype one --Nsubpro 125 --heat_redist No --roughness 1000
 # python  lib/Full_spectrum_main.py --id 11 --Ntheta 1 --Nwave 500 --LB 0.3 --UB 10 --mode TR --lavatype high --Nsubpro 250 --heat_redist No 
 
-# python  lib/Full_spectrum_main.py --id 11 --Ntheta 60 --Nwave 5 --LB 2.99 --UB 3.01 --mode PC --lavatype one --Nsubpro 125 --heat_redist No --roughness 0
-python  lib/Full_spectrum_main.py --id 12 --Ntheta 60 --Nwave 5 --LB 2.99 --UB 3.01 --mode PC --lavatype one --Nsubpro 125 --heat_redist No --roughness 1000
+python  lib/Full_spectrum_main.py --id 11 --Ntheta 60 --Nwave 5 --LB 2.99 --UB 3.01 --mode PC --lavatype one --Nsubpro 125 --heat_redist No --roughness 0
+# python  lib/Full_spectrum_main.py --id 12 --Ntheta 60 --Nwave 5 --LB 2.99 --UB 3.01 --mode PC --lavatype one --Nsubpro 125 --heat_redist No --roughness 1000
 # python  lib/Full_spectrum_main.py --id 11 --Ntheta 60 --Nwave 5 --LB 2.99 --UB 3.01 --mode PC --lavatype one --Nsubpro 125 --heat_redist No --roughness 0
 # python  lib/Full_spectrum_main.py --id 12 --Ntheta 60 --Nwave 5 --LB 2.99 --UB 3.01 --mode PC --lavatype one --Nsubpro 125 --heat_redist No --roughness 1000
 
