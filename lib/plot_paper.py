ff8d7dc4718e727fbc7dbb499d8b9172fb1241dd
dell <lhlphy@qq.com>
2025-02-25 01:21:08 +0800

    analytical solution 化简

--- lib/plot_paper.py
+++ lib/plot_paper.py
@@ -1,4 +1,4 @@
-# 该脚本绘制模型对比图，用于比较不同模型计算的结果 model_comparison.pdf
+# 该脚本绘制模型对比图, 用于比较不同模型计算的结果 model_comparison.pdf
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.interpolate import interp1d
@@ -61,7 +61,7 @@ def specular_diffuse_plot(name_specular, name_diffuse, Obs_wave, transit = 'off'
     Is_specular, Ii_specular, Id_specular, It_specular, theta = data_loader(name_specular, Obs_wave)
     Is_diffuse, Ii_diffuse, Id_diffuse, It_diffuse, theta = data_loader(name_diffuse, Obs_wave)
     
-    i = 0  # specular 与 diffuse与波长无关，仅取决于几何结构，所以i可以任意选取
+    i = 0  # specular 与 diffuse与波长无关, 仅取决于几何结构, 所以i可以任意选取
     theta = theta/(2 *np.pi)
 
     fig, ax = plt.subplots(figsize=(9,6))
@@ -90,34 +90,54 @@ def specular_diffuse_plot(name_specular, name_diffuse, Obs_wave, transit = 'off'
     plt.savefig(f"temp/{name_diffuse}/specular_diffuse_{Obs_wave[0]*1e6}_{transit}.png")
     plt.close()
  
-def analytical_theory_cal(transit = 'on'):
+def analytical_theory_cal(transit = 'on', An = 1, Fresnel = 'on', alpha = 0, Rp2Rs = 0):
     '''
-    计算解析近似理论的phase curve
+    计算解析近似理论的phase 
+    analytical_theory_cal中的A默认为1, 需要乘以由An决定的Fresnel系数
+    transit: 是否考虑transit修正, 默认为'on'
+    alpha: 可以自定义, 如果不指定, 则调用PPs计算
+    Rp2Rs: Rp/Rs 可以自定义, 如果不指定, 则调用PPs计算
+    
+    attention: analytical model 仅alpha, Rp2Rs涉及到系统参数, 若不自行指定, 则调用PPs计算
     '''
     from parameter_list import PPs
+    if (alpha == 0) and (Rp2Rs == 0):
+        alpha = np.arcsin(PPs.Rs / (PPs.semi_axis - PPs.Rp))
+        Rp2Rs = PPs.Rp / PPs.Rs
+    
     Theta = np.linspace(0, 2*np.pi, 200)
     RESULT = np.zeros((200, 2))
     RESULT[:,0] = Theta
     
-    alpha = np.arcsin(PPs.Rs / (PPs.semi_axis - PPs.Rp))
     deltaphi = alpha
     phi0 = (Theta + np.pi)/2
     theta0 = 0
     deltatheta = np.arccos(1-2*(np.cos(alpha)-1)/(np.cos(Theta)-1))
-    F = -PPs.Rp**2 / PPs.Rs**2 /8 *(deltatheta + np.sin(deltatheta)) * (np.sin(phi0+ deltaphi/2) - np.sin(phi0- deltaphi/2))
+    print('alpha:', alpha)
+    
+    # F = -Rp2Rs**2 /8 *(deltatheta + np.sin(deltatheta)) * (np.sin(phi0+ deltaphi/2) - np.sin(phi0- deltaphi/2)) # original解析近似理论(未完全化简)
+    F = Rp2Rs**2 *alpha**2 /4 *(1 - alpha**2 /24 *(2-np.cos(Theta)) /np.sin(Theta/2)**2) # 化简后的解析近似理论
+    
+    if Fresnel == 'on':  # 对于Fresnel模型, 需要乘以Fresnel系数
+        F = F * PPs.A_Fresnel(I_angle= (np.pi-Theta)/2, A_normal= An)
+    else:      # 若不考虑Fresnel效应, 直接乘以恒定的反射率常数 An
+        F = F * An
     
     if transit == 'on': # transit修正, 将 pi-alpha<Theta<pi+alpha 的值置为0
-        alpha = np.arcsin(PPs.Rs / (PPs.semi_axis - PPs.Rp))
         F[(np.pi - alpha <Theta) & (Theta < np.pi + alpha)] =0
         RESULT[:,1] = F
     elif transit == 'off':
-        RESULT[:,1] = F
-    return RESULT
+        RESULT[:,1] = F 
+        
+    return RESULT 
    
-def specular_diffuse_plot_theory(name_specular, name_diffuse, Obs_wave, transit = 'off', FR0 = 1):
+def specular_diffuse_plot_theory(name_specular, name_diffuse, Obs_wave, transit = 'off', An = 1):
     '''
     绘制模拟的diffuse和specular的phase curve, 以及解析近似理论和光学理论结果, 绘图中包含4曲线
     分别是： 模拟的specular和diffuse的phase curve, 解析近似理论和光学理论结果
+    An: 正入射的反射率, 本模型使用的Lambert模型默认反射率为1, 因此I_lambert需要乘以An; 
+        specular模型若使用“Fresnel 0.2copy"这类, 其中已经包含了An=0.2的修正, 因此不需要乘以An;
+        analytical_theory_cal中已经计算了Fresnel系数
     '''
     from parameter_list import PPs
     Is_specular, Ii_specular, Id_specular, It_specular, theta = data_loader(name_specular, Obs_wave)
@@ -128,34 +148,34 @@ def specular_diffuse_plot_theory(name_specular, name_diffuse, Obs_wave, transit 
 
     fig, ax = plt.subplots(figsize=(9,6))
     if transit == 'off':
-        ax.plot(theta, Id_diffuse[i,:]* 3/2 *1e6, label='Lambert', color='k', linewidth=2)
+        ax.plot(theta, Id_diffuse[i,:]* 3/2 *1e6 *An, label='Lambert', color='k', linewidth=2)
         ax.plot(theta, Is_specular[i,:] *1e6, label='Numerical', color='b', linewidth=2)
     else:
-        # thermal同时包含了thermal emission 和transit的修正项， 前者为正值或0，后者为负值
-        # 对于specualr_only and lambert_only模型, 这一步并不必要，因为本来就没有计算thermal emission，整个thermal 都是transit的修正项
-        # 但如果不小心使用了非only的模型，那么这一步就是必要的， 需要将thermal emission 去除，只保留transit的修正项
-        It_diffuse[It_diffuse > 0] = 0   # 当然，直接置为零会带来一定的误差，但考虑到(thermal emission << transit)，这个误差是可以接受的
+        # thermal同时包含了thermal emission 和transit的修正项,  前者为正值或0, 后者为负值
+        # 对于specualr_only and lambert_only模型, 这一步并不必要, 因为本来就没有计算thermal emission, 整个thermal 都是transit的修正项
+        # 但如果不小心使用了非only的模型, 那么这一步就是必要的,  需要将thermal emission 去除, 只保留transit的修正项
+        It_diffuse[It_diffuse > 0] = 0   # 当然, 直接置为零会带来一定的误差, 但考虑到(thermal emission << transit), 这个误差是可以接受的
         It_specular[It_specular > 0] = 0
-        ax.plot(theta, (Id_diffuse[i,:] + It_diffuse[i,:])*3/2 *1e6, label='Lambert', color='k', linewidth=2)
+        ax.plot(theta, (Id_diffuse[i,:] + It_diffuse[i,:])*3/2 *1e6 *An, label='Lambert', color='k', linewidth=2)
         ax.plot(theta, (Is_specular[i,:] + It_specular[i,:]) *1e6, label='Numerical', color='b', linewidth=2)
         
-    theory1 = analytical_theory_cal()
+    theory1 = analytical_theory_cal(An=0.2)
     # set all NAN to 0
     theory1 = np.nan_to_num(theory1, nan=0)
     # print(theory1)
     # theory1 = np.loadtxt('theory1.txt', delimiter = ',')
-    ax.plot(theory1[:,0]/(2*np.pi), theory1[:,1] * 1e6 *FR0, label='Analytical', color='r', linewidth=2, linestyle='--')
+    ax.plot(theory1[:,0]/(2*np.pi), theory1[:,1] * 1e6, label='Analytical', color='r', linewidth=2, linestyle='--')
     print('theory1:', theory1[(theory1.shape[0])//2, 1] *1e6)
     
-    # 绘制一条平行于x轴的直线，颜色为'gray'，线宽为1
+    # 绘制一条平行于x轴的直线, 颜色为'gray', 线宽为1
     theory2 = (PPs.Rp/2/PPs.semi_axis)**2 *1e6 # 21.1722  #21.3234
-    ax.axhline(y=theory2 * FR0, color='gray', linestyle='--', linewidth=2, label = 'Optical')
+    ax.axhline(y=theory2 * An, color='gray', linestyle='--', linewidth=2, label = 'Optical')
     # ax.plot((0, theory2), (1, theory2), color='gray', linestyle='--', linewidth=2, label = 'virtual image')
     
     ax.set_xlabel('Orbital phase', fontsize=18)
     ax.set_ylabel(r'$F_p/F_*$ (ppm)', fontsize=18)
     ax.set_xlim(0, 1)
-    ax.set_ylim(0, np.max([np.max((Id_diffuse[i,:] + It_diffuse[i,:])*3/2), np.max((Is_specular[i,:] + It_specular[i,:])), np.max(theory1[:,1] *FR0)]) *1e6 *1.05)
+    ax.set_ylim(0, np.max([np.max((Id_diffuse[i,:] + It_diffuse[i,:])*3/2 *An), np.max((Is_specular[i,:] + It_specular[i,:])), np.max(theory1[:,1])]) *1e6 *1.05)
     ax.spines['bottom'].set_linewidth(2)    ###设置底部坐标轴的粗细
     ax.spines['left'].set_linewidth(2)  ####设置左边坐标轴的粗细
     ax.spines['right'].set_linewidth(2) ###设置右边坐标轴的粗细
@@ -198,7 +218,7 @@ def polarization_ploter(FR0 = 0.1, Obs_wave = np.array([3]) * 1e-6, transit = 'o
         ax.plot(theta, Is_S *1e6, label='S-polarization', color=P_color, linewidth=2.5, linestyle='dashed')
         ax.plot(theta, (Is_S + Is_P) *1e6, label='Net-polarization', color='k', linewidth=2.5)
     else:
-        It_P[It_P > 0] =0   # 当然，直接置为零会带来一定的误差，但考虑到(thermal emission << transit)，这个误差是可以接受的
+        It_P[It_P > 0] =0   # 当然, 直接置为零会带来一定的误差, 但考虑到(thermal emission << transit), 这个误差是可以接受的
         It_S[It_S > 0] =0
         ax.plot(theta, (Is_P + It_P) *1e6, label='P-polarization', color=P_color, linewidth=2.5, linestyle='solid')
         ax.plot(theta, (Is_S + It_S) *1e6, label='S-polarization', color=P_color, linewidth=2.5, linestyle='dashed')
@@ -214,7 +234,7 @@ def polarization_ploter(FR0 = 0.1, Obs_wave = np.array([3]) * 1e-6, transit = 'o
         if transit == 'off':
             ax.plot(theta, Is_no *1e6, label='Net-polarization', color='b', linewidth=2.5, linestyle='dashed')
         else:
-            It_no[It_no > 0] =0   # 当然，直接置为零会带来一定的误差，但考虑到(thermal emission << transit)，这个误差是可以接受的
+            It_no[It_no > 0] =0   # 当然, 直接置为零会带来一定的误差, 但考虑到(thermal emission << transit), 这个误差是可以接受的
             ax.plot(theta, (Is_no + It_no) *1e6, label='Net-polarization', color='b', linewidth=2.5, linestyle='dashed')
             
     ax.set_xlabel('Orbital phase', fontsize=20)
@@ -231,7 +251,7 @@ def polarization_ploter(FR0 = 0.1, Obs_wave = np.array([3]) * 1e-6, transit = 'o
     ax.spines['top'].set_linewidth(2)   ####设置上部坐标轴的粗细
     #刻度值字体大小设置（x轴和y轴同时设置）
     plt.tick_params(labelsize=18)
-    if FR == 0.1:  # 仅在FR=0.1时绘制legend，4个子图只需要一个legend
+    if FR == 0.1:  # 仅在FR=0.1时绘制legend, 4个子图只需要一个legend
         plt.legend(fontsize=20, frameon=False)
     
     # set different info for .pdf name
@@ -288,10 +308,10 @@ def surface_model_compare(name_specular, name_diffuse, name_Fresnel, Obs_wave, t
         ax.plot(thetas, Is_specular[i,:] *1e6, label='Specular', color='b', linewidth=2.5)
         ax.plot(thetaf, Is_Fresnel[i,:] *1e6, label='Fresnel', color=F_color, linewidth=2.5)
     else:
-        # thermal同时包含了thermal emission 和transit的修正项， 前者为正值或0，后者为负值
-        # 对于specualr_only and lambert_only模型, 这一步并不必要，因为本来就没有计算thermal emission，整个thermal 都是transit的修正项
-        # 但如果不小心使用了非only的模型，那么这一步就是必要的， 需要将thermal emission 去除，只保留transit的修正项
-        It_diffuse[It_diffuse > 0] =0   # 当然，直接置为零会带来一定的误差，但考虑到(thermal emission << transit)，这个误差是可以接受的
+        # thermal同时包含了thermal emission 和transit的修正项,  前者为正值或0, 后者为负值
+        # 对于specualr_only and lambert_only模型, 这一步并不必要, 因为本来就没有计算thermal emission, 整个thermal 都是transit的修正项
+        # 但如果不小心使用了非only的模型, 那么这一步就是必要的,  需要将thermal emission 去除, 只保留transit的修正项
+        It_diffuse[It_diffuse > 0] =0   # 当然, 直接置为零会带来一定的误差, 但考虑到(thermal emission << transit), 这个误差是可以接受的
         It_specular[It_specular > 0] =0
         It_Fresnel[It_Fresnel > 0] =0
         ax.plot(thetad, (Id_diffuse[i,:] + It_diffuse[i,:])*FR *3/2 *1e6, label='Lambert', color='k', linewidth=2.5)
@@ -322,7 +342,7 @@ def surface_model_compare(name_specular, name_diffuse, name_Fresnel, Obs_wave, t
     ax.spines['top'].set_linewidth(2)   ####设置上部坐标轴的粗细
     #刻度值字体大小设置（x轴和y轴同时设置）
     plt.tick_params(labelsize=18)
-    if FR == 0.1:  # 仅在FR=0.1时绘制legend，4个子图只需要一个legend
+    if FR == 0.1:  # 仅在FR=0.1时绘制legend, 4个子图只需要一个legend
         plt.legend(fontsize=20, frameon=False)
     plt.savefig(f"temp/{name_Fresnel}/Fresnel{FR}.png")
     plt.savefig(f"temp/{name_Fresnel}/Fresnel{FR}.pdf")
@@ -330,8 +350,9 @@ def surface_model_compare(name_specular, name_diffuse, name_Fresnel, Obs_wave, t
     
 if __name__ == "__main__":
     # specular_diffuse_plot("R8copy", "R6copy", np.array([3]) * 1e-6, transit='off')
-    # 在使用transit='on'时，注意'R1'和'R2'位置上的PC必须经过 transit_cal.py 的计算；应该为'R1copy'和'R2copy'的形式
-    specular_diffuse_plot_theory("specular_copy", "lambert_copy", np.array([3]) * 1e-6, transit='on')
+    # 在使用transit='on'时, 注意'R1'和'R2'位置上的PC必须经过 transit_cal.py 的计算；应该为'R1copy'和'R2copy'的形式
+    # specular_diffuse_plot_theory("specular_copy", "lambert_copy", np.array([3]) * 1e-6, transit='on')
+    specular_diffuse_plot_theory("Fresnel 0.2copy", "lambert_copy", np.array([3]) * 1e-6, transit='on', An = 0.2)
     # specular_diffuse_plot_theory("R1copy", "R1copy", np.array([3]) * 1e-6, transit='on', FR0= 0.1)
     
     # ### 从plasma色图中均匀取出4个颜色
